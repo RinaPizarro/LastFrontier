a18f0ea3c6009d14dc93a4c1ec74a14b74a7d2c2
Karina <Pizarrorina@gmail.com>
2026-09-10 21:06:05 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import sql_server as s
+
+def main():
+    print("Let's connect to the LastFrontier database.")
+    while True:
+        user_host = input("Enter host: ")
+        user_name = input("Enter username: ")
+        user_password = input("Enter password: ")
+
+        status, message = s.connection(
+            username=user_name,
+            password=user_password,
+            host=user_host)
+
+        if status == False:
+            print(message)
+            print("Let's try that again. ")
+            continue
+        else:
+            print(message)
+            break
+
+if __name__ == "__main__":
+    main()
---
+++ sql_server.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+# Establish connection with sql server
+def connection(username, password, host):
+    try: 
+        connection = psycopg2.connect(
+            host=host,
+            database="LastFrontier",
+            user=username,
+            password=password,
+            port=5432
+        )
+        return True, "Connected successfully!"
+    
+    except psycopg2.OperationalError as e:
+        return False, f"Connection failed: {e}"
