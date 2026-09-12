3aaadf20c061dbe822d5c0403adca8183a5544dc
Karina <Pizarrorina@gmail.com>
2026-09-11 23:06:56 -0400

    made changed to find_table()

--- sql_server.py
+++ sql_server.py
@@ -16,8 +16,8 @@ def connection(username, password, host):
     except psycopg2.OperationalError as e:
         return False, f"Connection failed: {e}"
 
-def find_table():
-    pass
+def find_table(db_connection, table_name):
+    cursor = db_connection.cursor()
 
 def create_table(db_connection, table_name, columns_dict):
     try:
