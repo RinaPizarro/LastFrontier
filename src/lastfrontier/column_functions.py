0a9d770582b784298717533fd6bbb2cad7adf39b
Karina <Pizarrorina@gmail.com>
2026-09-17 15:58:42 -0800

    -

--- src/lastfrontier/column_functions.py
+++ src/lastfrontier/column_functions.py
@@ -7,30 +7,72 @@ def universal_time():
     )
 
 
-def output_headers_list(output):
-    columns_headers = ["time_utc"]
+def output_records_list(output):
+
+    if isinstance(output, dict):
+        return [output]
+
+    if isinstance(output, list):
+        return [
+            item
+            for item in output
+            if isinstance(item, dict)
+        ]
+
+    return []
+
+
+def output_to_columns(output, parent_key=""):
+
+    columns = {}
 
     for key, value in output.items():
-        if key == "weather" and isinstance(value, list) and value:
-            for weather_key in value[0].keys():
-                columns_headers.append(f"weather_{weather_key}")
-        else:
-            columns_headers.append(key)
 
-    return columns_headers
+        column_name = (
+            f"{parent_key}_{key}"
+            if parent_key
+            else key
+        )
+
+        if isinstance(value, dict):
+            columns.update(
+                output_to_columns(value, column_name)
+            )
+
+        elif isinstance(value, list):
+
+            if value and isinstance(value[0], dict):
+                columns.update(
+                    output_to_columns(
+                        value[0],
+                        column_name
+                    )
+                )
+
+        else:
+            columns[column_name] = value
+
+    return columns
+
+
+def output_headers_list(output):
+
+    columns = output_to_columns(output)
+
+    return [
+        "time_utc",
+        *columns.keys()
+    ]
 
 
 def output_values_list(output):
-    column_values = [universal_time()]
 
-    for key, value in output.items():
-        if key == "weather" and isinstance(value, list) and value:
-            for weather_value in value[0].values():
-                column_values.append(weather_value)
-        else:
-            column_values.append(value)
+    columns = output_to_columns(output)
 
-    return column_values
+    return [
+        universal_time(),
+        *columns.values()
+    ]
 
 
 def output_to_dict(headers_output, values_output):
@@ -47,18 +89,3 @@ def output_to_dict(headers_output, values_output):
             values_output
         )
     )
-
-
-def output_records_list(output):
-
-    if isinstance(output, dict):
-        return [output]
-
-    if isinstance(output, list):
-        return [
-            item
-            for item in output
-            if isinstance(item, dict)
-        ]
-
-    return []
